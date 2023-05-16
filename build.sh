49e630262a6b0a25a1817b15e45559cf35008784
Fabian Wiesel <fabian.wiesel@sap.com>
2023-05-16 10:24:49 +0200

    Switch to pyproject/ruff
    Pyproject.toml: PEP 621
    So we do not have a bunch of config-files flying around.
    
    Ruff is quite a bit faster and comprehensive than flake
    
    Generate a pbr.json to keep our changelog logic happy.
    
    pbr isn't yet 100% compatible with PEP 621 and doesn't
    seem to generate the pbr.json anymore by default.
    
    While we are at it, add a `--auto-remove` to the purge,
    so we also remove the automatically installed dependencies

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
-set -x
-set -e
+set -euxo pipefail
 
 apt-get update
 apt-get install -y gcc libssl-dev libssl1.* git
 pip install --no-cache-dir -e .
-apt-get purge -y gcc libssl-dev
+
+# "Fake" a pbr.json to keep our changelog happy
+[ -f vcenter_operator.egg-info/pbr.json ] || (
+  cat >vcenter_operator.egg-info/pbr.json <<EOT
+{"git_version": "$(git rev-parse --short HEAD)", "is_release": false}
+EOT
+)
+
+apt-get purge --autoremove -y gcc libssl-dev
 rm -r /var/lib/apt/lists /var/cache/apt/archives
-mkdir -p /var/cache/apt/archives
-mkdir -p /var/lib/apt/lists
+mkdir -p /var/cache/apt/archives /var/lib/apt/lists
